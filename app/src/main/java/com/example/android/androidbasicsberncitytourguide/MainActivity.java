e69a12deec4c6f2a2978af534eba6e8c0043c050
Thomas Kolonko <tz.kolonko@gmail.com>
2020-02-27 21:01:43 +0100

    Adding basic viewPager capability to app
    Following: https://developer.android.com/guide/navigation/navigation-swipe-view

--- app/src/main/java/com/example/android/androidbasicsberncitytourguide/MainActivity.java
+++ app/src/main/java/com/example/android/androidbasicsberncitytourguide/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.android.androidbasicsberncitytourguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//
+//        DemoCollectionPagerAdapter adapter = new DemoCollectionPagerAdapter(
+//                getSupportFragmentManager());
+//
+//        viewPager.setAdapter(adapter);
+//
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+//
+//        tabLayout.setupWithViewPager(viewPager);
     }
 }
