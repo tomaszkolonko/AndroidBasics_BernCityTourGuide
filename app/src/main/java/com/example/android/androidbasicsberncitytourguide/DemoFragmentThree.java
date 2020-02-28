5cb699c300cb7c58a7ed0ecc5dee661870f72cbf
Thomas Kolonko <tz.kolonko@gmail.com>
2020-02-28 16:10:29 +0100

    Added and implemented sound files for the club sound samples

--- app/src/main/java/com/example/android/androidbasicsberncitytourguide/DemoFragmentThree.java
+++ app/src/main/java/com/example/android/androidbasicsberncitytourguide/DemoFragmentThree.java
@@ -1,6 +1,8 @@
 package com.example.android.androidbasicsberncitytourguide;
 
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -9,6 +11,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 /**
@@ -22,6 +26,53 @@ import android.view.ViewGroup;
 public class DemoFragmentThree extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+
+    /** Handels the playback of the provided soundfiles */
+    private MediaPlayer mMediaPlayer;
+
+    /** Handles the AudioFocus */
+    private AudioManager mAudioManager;
+
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
+
+    /**
+     * This listener gets triggered whenever the audio focus changes
+     * (i.e., we gain or lose audio focus because of another app or device).
+     */
+    private AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                // both cases the same way because our app is playing short sound files.
+
+                // Pause playback and reset player to the start of the file. That way, we can
+                // play the word from the beginning when we resume playback.
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+            } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                mMediaPlayer.start();
+            } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                // Stop playback and clean up resources
+                releaseMediaPlayer();
+            }
+        }
+    };
 
     public DemoFragmentThree() {
         // Required empty public constructor
@@ -52,7 +103,112 @@ public class DemoFragmentThree extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_demo_fragment_three, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_demo_fragment_three, container, false);
+
+        // Create and setup the AudioManager to request audio focus
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
+        Button kapitelPlaySoundButton = rootView.findViewById(R.id.button_kapitel_sound_sample);
+        Button leCielPlaySoundButton = rootView.findViewById(R.id.button_leciel_sound_sample);
+        Button duTheatrePlaySoundButton = rootView.findViewById(R.id.button_dutheatre_sound_sample);
+
+        kapitelPlaySoundButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // release MediaPlayer resources BEFORE the MediaPlayer is initialized
+                releaseMediaPlayer();
+
+                // Request audio focus so in order to play the audio file. The app needs to play a
+                // short audio file, so we will request audio focus with a short amount of time
+                // with AUDIOFOCUS_GAIN_TRANSIENT.
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        // use the music stream
+                        AudioManager.STREAM_MUSIC,
+                        // request permanent focus
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // We have the audio focus
+
+                    // Create and setup the {@link MediaPlayer} for the audio resource associated
+                    // with the current word
+                    mMediaPlayer = MediaPlayer.create(getActivity(), R.raw.kapitel_sound_sample);
+
+                    // Start the audio file
+                    mMediaPlayer.start();
+
+                    // Setup a listener on the media player, so that we can stop and release the
+                    // media player once the sound has finished playing.
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+        });
+
+        leCielPlaySoundButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // release MediaPlayer resources BEFORE the MediaPlayer is initialized
+                releaseMediaPlayer();
+
+                // Request audio focus so in order to play the audio file. The app needs to play a
+                // short audio file, so we will request audio focus with a short amount of time
+                // with AUDIOFOCUS_GAIN_TRANSIENT.
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        // use the music stream
+                        AudioManager.STREAM_MUSIC,
+                        // request permanent focus
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // We have the audio focus
+
+                    // Create and setup the {@link MediaPlayer} for the audio resource associated
+                    // with the current word
+                    mMediaPlayer = MediaPlayer.create(getActivity(), R.raw.leciel_sound_sample);
+
+                    // Start the audio file
+                    mMediaPlayer.start();
+
+                    // Setup a listener on the media player, so that we can stop and release the
+                    // media player once the sound has finished playing.
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+        });
+
+        duTheatrePlaySoundButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // release MediaPlayer resources BEFORE the MediaPlayer is initialized
+                releaseMediaPlayer();
+
+                // Request audio focus so in order to play the audio file. The app needs to play a
+                // short audio file, so we will request audio focus with a short amount of time
+                // with AUDIOFOCUS_GAIN_TRANSIENT.
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        // use the music stream
+                        AudioManager.STREAM_MUSIC,
+                        // request permanent focus
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    // We have the audio focus
+
+                    // Create and setup the {@link MediaPlayer} for the audio resource associated
+                    // with the current word
+                    mMediaPlayer = MediaPlayer.create(getActivity(), R.raw.dutheatre_sound_sample);
+
+                    // Start the audio file
+                    mMediaPlayer.start();
+
+                    // Setup a listener on the media player, so that we can stop and release the
+                    // media player once the sound has finished playing.
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
+            }
+        });
+
+        return rootView;
     }
 
     /**
@@ -68,5 +224,38 @@ public class DemoFragmentThree extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // When the activity is stopped, release the MediaPlayer's resources even
+        // if the audiofile didn't finish playing
+        releaseMediaPlayer();
+    }
+
+    /**
+     * Clean up the media player by releasing it's resourced
+     */
+    private void releaseMediaPlayer() {
+        if(mMediaPlayer != null) {
+            // Regardless of the state of the mMediaPlayer, release its resources
+            // because we don't need it anymore
+            mMediaPlayer.release();
+
+            // Set the media player to null. For our project, we decided that having
+            // the mMediaPlayer set to null is an easy way to decide that it is not
+            // being used playing music or used in another state.
+            mMediaPlayer = null;
+
+            // Regardless of whether or not we were granted audio focus, we abandon it here.
+            // This also unregisters the AudioFocusChangeListener so we don't get anymore callbacks.
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+
+            // Show simple Toast message (dev only)
+            Toast.makeText(getActivity(), "Released MediaPlayer",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
