3c11099f7097aa3a2f23b4a86d9f6863c22a5a67
Thomas Kolonko <tz.kolonko@gmail.com>
2020-02-29 16:02:00 +0100

    current state on recyclerView

--- app/src/main/java/com/example/android/androidbasicsberncitytourguide/MainActivity.java
+++ app/src/main/java/com/example/android/androidbasicsberncitytourguide/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.android.androidbasicsberncitytourguide;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,22 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager(), this);
 
         viewPager.setAdapter(adapter);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.restaurant_recycler_view);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        Restaurant[] myRestaurants = new Restaurant[5];
+        myRestaurants[0] = new Restaurant("resti one", "lentulusrain 7", "9238592", "adsf@asdf.com");
+        myRestaurants[1] = new Restaurant("resti one", "lentulusrain 7", "9238592", "adsf@asdf.com");
+        myRestaurants[2] = new Restaurant("resti one", "lentulusrain 7", "9238592", "adsf@asdf.com");
+        myRestaurants[3] = new Restaurant("resti one", "lentulusrain 7", "9238592", "adsf@asdf.com");
+        myRestaurants[4] = new Restaurant("resti one", "lentulusrain 7", "9238592", "adsf@asdf.com");
+
+
+        mAdapter = new RestaurantAdapter(myRestaurants);
+        recyclerView.setAdapter(mAdapter);
 //
 //        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
 //
